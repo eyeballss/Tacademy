026fd2ff53452652180f12491ad001bf39497288
eyeballss <kkvv1111@gmail.com>
2017-07-20 11:55:24 +0900

    set recyclerview and its item layout on cardview

--- GPS project/Android/main/java/me/blog/eyeballss/gpstest/MainActivity.java
+++ GPS project/Android/main/java/me/blog/eyeballss/gpstest/MainActivity.java
@@ -104,6 +104,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setVisibility(View.GONE);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
